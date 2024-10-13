f294b0b13100c560fb066522caea4ba05ecb8eca
Thant Sin Linn <liam75260@example.com>
2024-10-13 10:14:40 +0630

    first commit

---
+++ yaycha/src/App.jsx
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import Form from "./Form.jsx";
+import Item from "./Item.jsx";
+import List from "./List.jsx";
+export default function App(){
+
+  const[data, setData] = useState([
+    { id: 1, content: "Hello, World!", name: "Alice" },
+    { id: 2, content: "React is fun.", name: "Bob" },
+    { id: 3, content: "Yay, interesting.", name: "Chris" }
+  ]);
+  const[showForm,setShowForm] = useState(false);
+
+  const remove = id=>{
+    setData(data.filter(item=>item.id !== id));
+  };
+
+  const add = (content,name)=>{
+    const id = data[data.length-1].id+1;
+    setData([...data,{id,content,name}]);
+  }
+  return (
+    <div style={{maxWidth: 600, margin: "20px auto"}}>
+      <h1
+      style={{
+        display: "flex",
+        justifyContent: "space-between",
+        alignItems: "center",
+      }}>
+        YayCha
+        <button
+        onClick={()=>setShowForm(!showForm)}
+        style={{
+          width: 32,
+          height: 32,
+          borderRadius: 50,
+          border: "0 none",
+          background: showForm ? "#dc3545" : "#0d6efd",
+          color: "white"
+        }}>
+         {showForm ? "x" : "+"}
+        </button>
+      </h1>
+     {showForm && <Form add={add}/>}
+      <List>
+        {data.map(item=>{
+          return(
+            <Item key={item.id} item={item} remove={remove}/>
+          );
+        })}
+      </List>
+    </div>
+  )
+}
